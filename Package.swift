c19fae595211ff5696c3ebae4f6c90e25286b28b
Marc Aupont <marc@lickability.net>
2019-07-23 12:51:21 -0400

    Updated vapor and leaf and added cloud.yml file for swift version update

--- Package.swift
+++ Package.swift
@@ -5,10 +5,10 @@ let package = Package(
     name: "TestSite",
     dependencies: [
         // 💧 A server-side Swift web framework.
-        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
+        .package(url: "https://github.com/vapor/vapor.git", from: "3.3.0"),
 
         // 🍃 An expressive, performant, and extensible templating language built for Swift.
-        .package(url: "https://github.com/vapor/leaf.git", from: "3.0.0"),
+        .package(url: "https://github.com/vapor/leaf.git", from: "3.0.2"),
     ],
     targets: [
         .target(name: "App", dependencies: ["Leaf", "Vapor"]),
